9c4f1633a77b871aaff78ae339bcf64f5a318993
Adrian <ageimperial@gmail.com>
2022-07-31 18:22:29 -0700

    added username/password

--- db/seed.js
+++ db/seed.js
@@ -11,7 +11,8 @@ const createInitialUsers = async () => {
             console.log("Starting to create users...");
 
             const albert = await createUser({username: 'albert', password: 'bertie99'});
-            const sandra = await createUser({username: 'sandra', password: 'glamgal'})
+            const sandra = await createUser({username: 'sandra', password: '2sandy4me'});
+            const glamgal = await createUser({username: 'glamgal', password: 'soglam'});
 
             console.log(albert);
             console.log("Finished creating users!");
