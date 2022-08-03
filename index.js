cc05dc2be2c87a16ae08a94a2a07cacb22bdd91d
Adrian <ageimperial@gmail.com>
2022-08-02 21:07:13 -0700

    added api

---
+++ index.js
@@ -0,0 +1,28 @@
+const PORT = 3000;
+const express = require('express');
+const morgan = require('morgan');
+const server = express(); 
+
+server.use(morgan('dev'));
+server.use(express.json())
+
+server.use((req, res, next)=> {
+    console.log("<____Body Logger START_____>");
+    console.log(req.body);
+    console.log("<____Body Logger END_____>");
+    
+    next();
+})
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+
+const { client } = require('./db');
+client.connect();
+
+
+
+server.listen(PORT, () => {
+    console.log('The server is up on port', PORT)
+});
