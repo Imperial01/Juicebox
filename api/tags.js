46e6ba90be96496797b19686d76bff9c56731997
Adrian <ageimperial@gmail.com>
2022-08-07 11:12:01 -0700

    day 3 finished

--- api/tags.js
+++ api/tags.js
@@ -2,13 +2,27 @@ const express = require('express');
 const tagsRouter = express.Router();
 
 const { getAllTags } = require('../db');
-
+const { getPostsByTagName } = require('../db')
 
 tagsRouter.get('/', async (req, res, next) => {
     const tags = await getAllTags(); 
     res.send({
         tags
     })
+})
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    try {
+        const posts = await getPostsByTagName(tagName);
+        res.send ({
+            posts
+        })
+    } catch ({ name, message }) {
+        res.send({
+            name: error.name,
+            message: error.message
+        })
+    }
 })
 
 
